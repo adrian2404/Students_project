0117a8e7c03a826fccb1155b930aa9ac9d25dbb2
Adrian Yavorskyy <adrian.yavorski@gmail.com>
2015-03-03 21:28:38 +0200

    migartions

---
+++ students/migrations/0004_auto_20150302_2127.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('students', '0003_group_leader'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='student',
+            name='student_group',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, verbose_name=b'\xd0\x93\xd1\x80\xd1\x83\xd0\xbf\xd0\xb0', to='students.Group', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='group',
+            name='leader',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, blank=True, to='students.Student', verbose_name=b'\xd0\xa1\xd1\x82\xd0\xb0\xd1\x80\xd0\xbe\xd1\x81\xd1\x82\xd0\xb0'),
+            preserve_default=True,
+        ),
+    ]
