d4a601808cae4d8618a7fdf63f5bb680a8962685
Adrian Yavorskyy <adrian.yavorski@gmail.com>
2015-11-03 22:43:06 +0200

    groups processor added

--- students/context_processors.py
+++ students/context_processors.py
@@ -1,4 +1,7 @@
-from .setting import PORTAL_URL
+#from .setting import PORTAL_URL
+from .util import get_groups
+#def students_proc(request):
+#	return {'PORTAL_URL':PORTAL_URL}
 
-def students_proc(request):
-	return {'PORTAL_URL':PORTAL_URL}
+def groups_processor(request):
+	return {'GROUPS': get_groups(request)}
