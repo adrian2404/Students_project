142580bcb33b16626529ad5f93dbdb3dbfa01349
Adrian Yavorskyy <adrian.yavorski@gmail.com>
2015-11-03 22:53:38 +0200

    teachers' cbv added

--- students/views/teachers.py
+++ students/views/teachers.py
@@ -3,153 +3,196 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+from ..models import Teacher
+from ..util import paginate
+
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from datetime import datetime
-#from django.view.generic import UpdateView
 from django.forms import ModelForm
-from django.views.generic import UpdateView
+from django.db.models.deletion import ProtectedError
 
-from ..models import Student, Group, Teacher
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, HTML
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.layout import Layout, Field
 
-# Create your views here.
-# Views for students
-def teachers_list(request):
-	teachers = Teacher.objects.all()
-	
-	# #ordering students 
-	# order_by=request.GET.get('order_by','')
-	# students=students.order_by('last_name')
-	# if request.GET.get('order_by', '') == '':
-	# 	request.GET.order_by = 'last_name'
-	# #order_by = request.GET.get('order_by', '')
-	
-	# if order_by in ('last_name','first_name','ticket','id'):
-	# 	students=students.order_by(order_by)
-	# 	if request.GET.get('reverse','')=='1':
-	# 		students=students.reverse()
-	
-	# #paginate students
-	# paginator = Paginator(students, 3)
+# def groups_list(request):
+#     groups = Group.objects.all()
+
+
+#     #ordering groups
+#     order_by = request.GET.get('order_by','')
+#     groups=groups.order_by('title')
+#     if request.GET.get('order_by','') == '':
+#         request.GET.order_by = 'title'
+
+#     if order_by in ('leader', 'id','title'):
+#         groups=groups.order_by(order_by)
+#         if request.GET.get('reverse','')=='1':
+#             groups = groups.reverse()
+
+	#pagination
+
+	# paginator = Paginator(groups, 3) # 
+
 	# page = request.GET.get('page')
 	# try:
-	# 	students = paginator.page(page)
+	#     groups = paginator.page(page)
 	# except PageNotAnInteger:
-	# 	# If page is not an integer, deliver first page.
-	# 	students = paginator.page(1)
+	#     # If page is not an integer, deliver first page.
+	#     groups = paginator.page(1)
 	# except EmptyPage:
-	# 	# If page is out of range (e.g. 9999), deliver last page of results.
-	# 	students = paginator.page(paginator.num_pages)
+	#     # If page is out of range (e.g. 9999), deliver last page of results.
+	#     groups = paginator.page(paginator.num_pages)
+
+	# context = paginate(groups, 4, request, {}, var_name='groups')
 
 
-	return render(request, 'students/teachers_list.html',
-		{'teachers': teachers})
+	# return render(request, 'students/groups_list.html',
+	#     context)
 
-def teachers_add(request):
-    return HttpResponse('<h1> Teacher add  </h1>')
-    # # If form was posted
-    # if request.method == "POST":
-    #     # was form add_button clicked
-    #     if request.POST.get('add_button') is not None:
+class TeacherList(ListView):
+	model = Teacher
+	context_object_name = 'teachers'
+	template_name = 'students/teachers_list.html'
 
-    #         data = {'middle_name':request.POST.get('middle_name'),'notes':request.POST.get('notes')}
-    #         #TODO: validate input errors from user
-    #         errors ={}
+	def get_context_data(self, **kwargs):
 
-    #         first_name=request.POST.get('first_name','').strip()
-    #         if not first_name:
-    #             errors['first_name'] = u"Ім'я є обов'язковим"
-    #         else:
-    #             data['first_name']=first_name 
+		
+		context = super(TeacherList, self).get_context_data(**kwargs)
 
-    #         last_name=request.POST.get('last_name','').strip()
-    #         if not last_name:
-    #             errors['last_name'] = u"Прізвище є обов'язковим"
-    #         else:
-    #             data['last_name']=last_name
+		context = paginate(self.object_list, 4, self.request, {}, var_name='teachers')
 
-    #         birthday=request.POST.get('birthday','').strip()
-    #         if not birthday:
-    #             errors['birthday'] = u"Дата народження є обов'язковою"
-    #         else:
-    #             try:
-    #                 datetime.strptime(birthday,'%Y-%m-%d')
-    #             except Exception:
-    #                 errors['birthday']=u"Введіть коректну дату(напр. 1984-12-30)"
+		return context
 
-    #             else:
-    #                 data['birthday']=birthday
+	def get_queryset(self):
+		teachers = Teacher.objects.all()
 
-    #         ticket=request.POST.get('ticket','').strip()
-    #         if not ticket:
-    #             errors['ticket'] = u"Номер білета є обов'язковим"
-    #         else:
-    #             data['ticket']=ticket
+		order_by = self.request.GET.get('order_by','')
+		if order_by == '':
+			self.request.GET.order_by = 'last_name'
+			teachers = teachers.order_by('last_name')
+		if order_by in ('first_name','last_name','subject', 'id'):
+			teachers=teachers.order_by(order_by)
+			if self.request.GET.get('reverse','') =='1':
+				teachers=teachers.reverse()
 
-    #         student_group=request.POST.get('student_group','').strip()
-    #         if not student_group:
-    #             errors['student_group'] = u"Група є обов'язкова"
-    #         else:
-    #             groups=Group.objects.filter(pk=student_group)
-    #             if len(groups)!=1:
-    #                 errors['student_group']=u"Оберіть коректну групу"
-    #             else:
-    #                 data['student_group']=groups[0]
-
-    #         formats = ['jpg','jpeg','bmp','png','gif','riff']
-
-    #         photo=request.FILES.get('photo')
-    #         if photo:
-    #             file_name = photo.name.split('.')
-    #             if photo.size > 2000000:
-    #                 errors['photo'] = u"Розмір файлу завеликий"
-    #             elif file_name[1] not in formats:
-    #                 errors['photo'] = u"Це має бути фото"
-    #             else:
-    #                 data['photo']=photo
-
-            
-
-    #         if not errors:
-    #             #create student
-    #             student=Student(**data)
-    #             #save student to database
-    #             student.save()
-
-    #             #redirect user to the students list
-
-    #             return HttpResponseRedirect(u'%s?status_message=Студента успішно додано'%reverse('home'))
-
-    #         else:
-    #             # render form with errors and previous user input
-    #             return render(request, 'students/students_add.html',{'groups':Group.objects.all().order_by('title'),
-    #                 'errors':errors})
-    #     elif request.POST.get('cancel_button') is not None:
-    #         #redirect to homepage
-
-    #         return HttpResponseRedirect(u'%s?status_message=Додавання студента відмінено'%reverse('home'))
-    # else:
-    #     #initial form render
-    #     return render(request,'students/students_add.html',{'groups':Group.objects.all().order_by('title')})
-
-    
-# class StudentUpdateView(UpdateView):
-#     model = Student
-#     template_name = 'students/students_edit.html'
-    
-
-#     @property
-#     def success_url(self):
-#         return u'%s?status_message=Студента успішно збережено'%reverse('home')
-
-#     def post(self,request, *args, **kwargs):
-#         if request.POST.get('cancel_button'):
-#             return HttpResponseRedirect(u'%s?status_message=Редагування студента відмінено'%reverse('home'))
-#         else:
-#             return super(StudentUpdateView, self).post(request,request, *args, **kwargs)
+		return teachers
 
 
-def teachers_edit(request,sid):
-    return HttpResponse('<h1> Teacher edit %s </h1>' % sid)
+class TeacherForm(ModelForm):
+	class Meta:
+		model=Teacher
+		fields = "__all__"
 
-def teachers_delete(request, sid):
-	return HttpResponse('<h1> Delete teacher %s </h1>' % sid)
+	def __init__(self, *args, **kwargs):
+		super(TeacherForm, self).__init__(*args, **kwargs)
+
+		self.helper = FormHelper(self)
+
+		if kwargs['instance'] is None:
+			add_form = True
+		else:
+			add_form = False
+
+		#set form tag attributes
+		if add_form:
+			self.helper.form_action = reverse('teachers_add')
+		else:
+			self.helper.form_action = reverse('teachers_edit', kwargs={'pk':kwargs['instance'].id})
+
+		self.helper.form_method = 'POST'
+		self.helper.form_class = 'form-horizontal'
+
+
+		break_message= u"Додавання викладача відмінено"
+		#  set form field properties
+
+		self.helper.help_text_inline = True
+		self.helper.html5_required = True
+		self.helper.label_class = 'col-sm-2 control-label'
+		self.helper.field_class = 'col-sm-10'
+
+		if add_form:
+			submit = Submit('add_button', u'Додати',css_class="btn btn-primary")
+			self.helper.layout[-1]= Layout(
+                Field('subject',css_class='input-xlarge',),
+                FormActions(submit,
+            HTML("<a class='btn btn-link' id = 'cancel-id-send_button' href='{% url 'teachers_list' %}?status_message="+break_message+"' name = 'cancel_button'>"+u'Скасувати'+"</a>"),
+            ))
+		else:
+			submit = Submit('save_button', u'Зберегти',css_class="btn btn-primary")
+			cancel = Submit('cancel_button', u'Відмінити',css_class="btn btn-link")
+			self.helper.layout[-1] = Layout(Field('subject',css_class='input-xlarge',),
+			FormActions(submit, cancel))
+
+class BaseTeacherFormView(object):
+
+	def get_success_url(self):
+		return u'%s?status_message=Зміни успішно збережені!'%reverse ('teachers_list')
+
+	def post(self, request, *args, **kwargs):
+		# import pdb; pdb.set_trace()
+		if request.POST.get('cancel_button'):
+			return HttpResponseRedirect(u'%s?status_message=Редагування викладача відмінено!'%reverse('teachers_list'))
+		else:
+			return super(BaseTeacherFormView, self).post(request, *args, **kwargs)
+
+
+class TeacherAddView(BaseTeacherFormView, CreateView):
+	model = Teacher
+	form_class = TeacherForm
+	template_name = 'students/teachers_universal.html'
+
+	def get_context_data(self, **kwargs):
+
+		context = super(TeacherAddView, self).get_context_data(**kwargs)
+		context['name'] = 'Додати'
+
+		return context
+
+
+class TeacherUpdateView(BaseTeacherFormView, UpdateView):
+	model = Teacher
+	form_class = TeacherForm
+	template_name = 'students/teachers_universal.html'
+
+
+
+	def get_context_data(self, **kwargs):
+		context = super(TeacherUpdateView, self).get_context_data(**kwargs)
+		context['name'] = 'Редагувати'
+
+		return context
+
+	# def post(self, request, *args, **kwargs):
+	#     # import pdb; pdb.set_trace()
+	#     if request.POST.get('cancel_button'):
+	#         return HttpResponseRedirect(u'%s?status_message=Редагування групи відмінено!'%reverse('groups_list'))
+	#     else:
+	#         return super(BaseGroupFormView, self).post(request, *args, **kwargs)
+		
+
+class TeacherDeleteView(BaseTeacherFormView, DeleteView):
+    model = Teacher
+    template_name = 'students/teachers_confirm_delete.html'
+
+    def post (self, request, *args, **kwargs):
+        if request.POST.get('cancel_button'):
+            return HttpResponseRedirect(u'%s?status_message=Видалення викладача скасовано'%reverse('teachers_list'))
+        else:
+            return super(ExamDeleteView, self).post(request,*args, **kwargs)
+
+		
+
+
+
+# def groups_add(request):
+# 	return HttpResponse('<h1> group add form</h1>')
+
+# def groups_edit(request, gid):
+# 	return HttpResponse('<h1> Edit Groups %s </h1>' %gid)
+
+# def groups_delete(request, gid):
+# 	return HttpResponse('<h1> Delete Groups %s </h1>' %gid)
