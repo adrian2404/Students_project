ef48f3ef7477ef3e91f25a8852fb50468c74fa03
Adrian Yavorskyy <adrian.yavorski@gmail.com>
2015-11-23 10:40:08 +0200

    Subject field deleted

--- students/models/teachers.py
+++ students/models/teachers.py
@@ -41,11 +41,6 @@ class Teacher(models.Model):
     null=True)
 
 
-    subject = models.CharField(
-    max_length=256,
-    blank=True,
-    null=True,
-    verbose_name=u"Предмет")
 
 
     def __unicode__(self):
